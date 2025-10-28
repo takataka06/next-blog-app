6067e263c76f25ba46c0f401e53178eb62505d45
takataka06 <reyreyrey0110@gmail.com>
2025-10-28 20:40:53 +0900

    prisma setting

---
+++ prisma/seed.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from "@prisma/client";
+
+import * as bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // クリーンアップ
+  await prisma.user.deleteMany();
+  await prisma.post.deleteMany();
+
+  const hashedPassword = await bcrypt.hash('password123', 10);
+
+  // ダミー画像のURL
+  const dummyImages = [
+    'https://picsum.photos/seed/post1/600/400',
+    'https://picsum.photos/seed/post2/600/400',
+  ]
+
+  const user = await prisma.user.create({
+    data: {
+      email: "test@example.com",
+      name: "Test User",
+      password: hashedPassword,
+      posts: {
+        create: [
+          {
+            title: "初めての投稿",
+            content: "これは私の最初のブログ投稿です！",
+            published: true,
+            topImage: dummyImages[0],
+          },
+          {
+            title: "2の投稿",
+            content: "これは私の2のブログ投稿です!",
+            published: true,
+            topImage: dummyImages[1],
+          }
+        ],
+      },
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
